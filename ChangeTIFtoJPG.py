2ec7cdc0e9d3152b11db5c7b5bfa194ce28726e2
Omcar <51676294+omkar-udawant@users.noreply.github.com>
2019-06-23 11:36:02 +0530

    Update ChangeTIFtoJPG.py

--- ChangeTIFtoJPG.py
+++ ChangeTIFtoJPG.py
@@ -1,15 +1,17 @@
 # coding: utf-8
+# author: Omkar 
+# Date: 22/06/2019 09:32:05
 
 from cv2 import imread, imwrite
 import os
 
-files = [file for file in os.listdir('300/') if file.endswith('.tif')]
+path_to_tig = 'TIF_dir/'
+files = [file for file in os.listdir(path_to_tif) if file.endswith('.tif')]
 
 out_path = 'jpg_images/'
 if not os.path.exists(out_path):
     os.mkdir(out_path)
 
 for f in files:
-    img = cv2.imread('300/'+f)
+    img = cv2.imread(path_to_tif + f)
     cv2.imwrite(out_path + f.split('.')[0] + '.jpg', img)
-
